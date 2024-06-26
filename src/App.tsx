12998d7516fe13945d3a2863d8c53b2d256afc15
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-06-26 12:23:13 +0300

    header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
+import { PageHeader } from "./layouts/PageHeader";
 
-function App() {
-	return <div className="App">Youtube</div>;
+export default function App() {
+	return (
+		<div className="max-h-screen flex flex-col">
+			<PageHeader />
+			<div>2</div>
+		</div>
+	);
 }
-
-export default App;
