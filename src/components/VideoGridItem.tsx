1a7c3c0a08fcf6bad2a6ce47eeeef79329b448d8
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-07-04 13:21:21 +0300

    utility function added to format duration with leading zeros

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,3 +1,5 @@
+import { formatDuration } from "../utils/formatDuration";
+
 type VideoGridItemProps = {
 	id: string;
 	title: string;
@@ -29,7 +31,7 @@ export function VideoGridItem({
 				<img src={thumbnailUrl} className="block w-full h-full object-cover rounded-xl" />{" "}
 				<div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
 					{" "}
-					{duration}
+					{formatDuration(duration)}
 				</div>
 			</a>
 		</div>
